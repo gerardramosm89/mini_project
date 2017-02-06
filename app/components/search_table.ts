d8625602aff0d228470b1a66d4bb4d436059f18c
root <gerard.ramosm89@gmail.com>
2017-02-06 15:38:13 -0800

    added form validation

--- app/components/search_table.ts
+++ app/components/search_table.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'search-bar',
   template:`
   <div class="col-xs-6 col-xs-offset-3" style="margin-bottom:30px">
     <form [formGroup]="myForm" class="input-group" (ngSubmit)="searchClick()">
-      <input type="text" class="form-control" formControlName="searchTerm">
+      <input type="text" placeholder="Enter a search term" class="form-control" formControlName="searchTerm">
       <span class="input-group-btn">
-        <button class="btn btn-info">Search</button>
+        <button class="btn btn-info" [disabled]="!myForm.valid">Search</button>
       </span>
     </form>
   </div>
@@ -37,14 +37,15 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 export class SearchComponent{
   myForm: FormGroup;
+  //Taking in data from the parent component
   @Input() data: any;
-  foundArray: any = [];
+  foundArray: any = null;
   searchTerm: any;
   n: number;
   currentName: String;
   constructor(){
     this.myForm = new FormGroup({
-      'searchTerm': new FormControl()
+      'searchTerm': new FormControl('', Validators.required)
     });
   }
   searchClick(){
@@ -53,7 +54,7 @@ export class SearchComponent{
     for (let i = 0;i < this.data.length;i++){
       this.currentName = this.data[i].name;
       this.n = this.currentName.indexOf(this.searchTerm);
-      console.log(this.n);
+      //decided to go with indexOf, because if we get back -1 it means we did not find an instance of that word
       if (this.n != -1){
         this.foundArray.push(this.data[i]);
         console.log(this.foundArray);
